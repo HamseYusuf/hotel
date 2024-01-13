cc5d7bb848aa147a97b9436ecba0bc0b3407adc1
HamseYusuf <xamseyuusuf83@gmail.com>
2024-01-13 21:44:33 +0300

    update

--- update.php
+++ update.php
@@ -1,15 +1,28 @@
-<?php 
-include("db.php");
+<html>
+    <?php include("db.php");
+          include("base.php");
+          include("header.php");
+         
+           if(isset($_GET['id'])) {
+            $id = $_GET['id'];
+            echo " we have got the id $id";
+           $stmt = $conn->prepare(" SELECT * FROM booking WHERE id=$id");
+           $stmt->execute();
+           $booking = $stmt->fetch(PDO::FETCH_ASSOC);
+           
+           }
+    ?>
 
-$id = $_GET['id'];
-$sql = "SELECT * FROM booking WHERE id = :id ";
-
-$stmt = $conn->prepare($sql);
-$stmt->bindParam("id" , $id);
-$stmt->execute();
-$booking = $stmt->fetch(PDO::FETCH_ASSOC);
-
-echo $booking['name'];
-echo $booking['phone'];
-
-?>
+    <body>
+        <div class="container">
+            <div class="row flex justify-content-center">
+                <form action="update_handle.php?id=<?php echo $id ?>"  method="post" class="form-group">
+                    <input type="text" name="booking_name" value="<?php echo $booking['name'] ?>" class="form-control">
+                    <input type="text" name="phone" value="<?php echo $booking['phone'] ?>" class="form-control">
+                    <input type="date" name="booking_date" value="<?php echo $booking['date'] ?>" class="form-control">
+                    <input type="submit" value="submit" class="form-control btn btn-info">
+                </form>
+            </div>
+        </div>
+    </body>
+</html>
---
+++ update_handle.php
@@ -0,0 +1,21 @@
+<?php
+include("db.php");
+$id= $_GET['id'];
+
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $name = $_POST['booking_name'];
+    $phone = $_POST['phone'];
+    $date = $_POST['booking_dateS'];
+        $sql = " UPDATE booking SET name = :name , phone = :phone , date = :date WHERE id = $id";
+        $stmt = $conn->prepare($sql);
+        $stmt->bindParam(":name" , $name);
+        $stmt->bindParam(":phone" , $phone);
+        $stmt->bindParam(":date" , $date);
+        $stmt->execute();
+        header("Location: index.php");
+
+    } else{
+        header("Location:form.php");
+    }
+
+?>
